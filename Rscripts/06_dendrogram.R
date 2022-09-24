3bb87411afa31af38c75877d66b6d087c39c5634
VeronicaNoe <54764940+VeronicaNoe@users.noreply.github.com>
2022-09-24 18:45:53 +0200

    Update 06_dendrogram.R

--- Rscripts/06_dendrogram.R
+++ Rscripts/06_dendrogram.R
@@ -1,36 +1,50 @@
+## Set path to input files:
 rm(list=ls())
 wd<-getwd()
-getwd()
 baseDir <- gsub("/results", "", wd)
 scriptDir <- file.path(baseDir, "scripts")
 
-## load packages silently
+## Load packages silently:
 suppressPackageStartupMessages({
-  library("data.table") # file reading
-  library("DSS")
-  library("qqman")
-  library("plyr")
-  library("dplyr")
+  library(data.table) # file reading
+  library(adegenet)
+  library(poppr)
   library(vegan)
-  library("reshape2")
-  library("tidyr")
-  library("tidyverse")
-  source(file.path(scriptDir, "commonFunctions.R"))
+  library(tidyr)
+  source(file.path(scriptDir, "commonFunctions.R"), local=TRUE)
 })
-# process both data set
-RE<-c("AseI", "Csp6")
+
+# for genetic data
+data<-read.csv(paste0(baseDir,"/rawData/SSR_data.csv"), header=TRUE, stringsAsFactors = FALSE, row.names = NULL, sep="\t")
+str(data)
+rownames(data)<-data$Pop
+ind<-as.character(data[,1])
+site<-as.character(data[,2])
+data[,1:3]<-NULL
+data_gen<-df2genind(data, ploidy = 3, ind.names = ind, pop=site, sep="_")
+# plot
+pdf(paste0(baseDir,"/plots/SSR_NeiDistance_UPGMA.pdf"))
+data_gen %>% 
+  genind2genpop() %>%
+  aboot(cutoff = 60, quiet = TRUE, sample = 1000, distance = nei.dist)
+dev.off()
+
+# for epigenetic data
+## process both data set
+RE<-c("AseI-NsiI", "Csp6I-NsiI")
 for (r in 1:length(RE)){
   designTable <- file.path(paste0(baseDir, "/rawData/",RE[r], "_Design_with_converage_info.txt"))
-  infileName <- file.path(paste0(baseDir,"/results/",RE[r],"_methylation.filtMETH"))
-  annotationFile <- file.path(paste0(baseDir, "/rawData/",RE[r], "_mergedAnnot.csv"))
+  infileName <- file.path(paste0(baseDir,"/rawData/",RE[r],"_methylation.filtered"))
+  annotationFile <- file.path(paste0(baseDir, "/annotation/",RE[r], "_mergedAnnot.csv"))
+  
   sampleTab <- f.read.sampleTable(designTable) # see commonFunctions.R
   myData <- f.load.methylation.bed(infileName) # see commonFunctions.R
   myData<-unite(myData, chrPos, c(chr, pos), sep="_", remove=FALSE)
   sort(myData$chrPos)
   rownames(myData)<-myData$chrPos
-
+  
   inFile<-list.files(pattern = "_table.csv")
-  temp<-read.csv(inFile, header=TRUE, sep="\t")
+  temp<-read.csv(inFile[1], header=TRUE, sep="\t")
   temp<-unite(temp, chrPos, c(chr, pos), sep="_", remove=FALSE)
   toKeepRE<-RE[r]
   temp<-dplyr::filter(temp, RE==toKeepRE)
@@ -38,17 +52,17 @@ for (r in 1:length(RE)){
   temp<-dplyr::filter(temp, pvals<=0.05)
   sort(temp$chrPos)
   rownames(temp)<-temp$chrPos
-
+  
   context <- c("CG", "CHH", "CHG")
   whichAnalysis<-c("all", "DMC")
   for (j in 1:length(context)){
     if (context[j] != "all") {
       contextFilter <- context[j]
       myD <- subset(myData, context == contextFilter)
-      } else if (context[j] == "all"){
+    } else if (context[j] == "all"){
       myD<-myData
-      }
-  # subset meth data that has DMC
+    }
+    # subset meth data that has DMC
     for (a in 1:length(whichAnalysis)){
       if(whichAnalysis[a]=="all"){
         mD<-myD
@@ -58,7 +72,7 @@ for (r in 1:length(RE)){
         mD<-myD[keep,]
         feature<-c("all",unique(temp$feature))
       }
-  ## a plot for each feature
+      ## a plot for each feature
       for (i in 1:length(feature)){
         subAnno <- f.load.merged.annotation(annotationFile, feature[i])
         toKeep <- gsub("chr", "", rownames(subAnno))
@@ -87,23 +101,11 @@ for (r in 1:length(RE)){
           dist<-vegdist(decostand(dis, "norm"), "euclidean", na.rm=TRUE)
           clus<-hclust(dist, "average")
           dirOut<-paste0(baseDir,"/plots/",RE[r],"_all" ,"_Context_methylationLevelsPerFeature.pdf")
-          pdf(paste0(baseDir,"/plots/",RE[r],"_","Dendrogram_","allSamples_",context[j],"_",whichAnalysis[a],"_", feature[i], ".pdf"))
+          pdf(paste0(baseDir,"/plots/",RE[r],"_","Dendrogram_","allSamples_",context[j],"_",whichAnalysis[a],"_", feature[i], "_2.pdf"))
           plot(clus, cex = 0.6, hang=-0.5,main=paste0("Dendrogram_",RE[r],"_",context[j],"_",whichAnalysis[a],"_", feature[i]))
           dev.off()
-          }
-       }
+        }
+      }
     }
   }
 }
-
-### SSR dendrogram
-data<-read.csv(paste0("<PATH_TO_FILE>/SSR_Data.csv"), header=TRUE, stringsAsFactors = FALSE, row.names = NULL, sep=",")
-str(data)
-rownames(data)<-data$Pop
-data$Pop<-NULL
-data.sim<-vegdist(data,  na.rm=TRUE, "euclid")
-clus<-hclust(data.sim, "average")
-pdf("Dendrogram.pdf")
-pdf(paste0(baseDir,"/plots/","SSR_Dendrogram.pdf"))
-plot(clus, hang = -1, cex = 0.6, main="SSR_dendrogram", ylab="Euclidean distance")
-dev.off()
